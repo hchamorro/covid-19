0038a66f89be5e6d76e1c9ba94cfd842eb117d9c
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-04-03 20:26:54 -0500

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello World</div>;
+  return <Router> </Router>;
 }
 
 export default App;
