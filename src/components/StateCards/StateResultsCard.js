817de1d773e4b916275b36918da7eb0d3471a738
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-04-10 13:41:15 -0500

    number function

--- src/components/StateCards/StateResultsCard.js
+++ src/components/StateCards/StateResultsCard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { StateDataContext } from '../../utils/StateData';
+import formatNumber from '../formatNumber';
 
 function StateResults() {
   const [stateData, setStateData] = useContext(StateDataContext);
@@ -11,38 +12,44 @@ function StateResults() {
           <div>
             <div>
               <div>
-                <strong>{stateData[0].total} </strong>people have been tested.
+                <strong>{formatNumber(stateData[0].total)} </strong>people have
+                been tested.
               </div>
               <div>
-                <strong>{stateData[0].positive}</strong> people have tested
-                positive.
+                <strong>{formatNumber(stateData[0].positive)}</strong> people
+                have tested positive.
               </div>
               {stateData[0].hospitalizedCumulative ? (
                 <div>
-                  <strong>{stateData[0].hospitalizedCumulative}</strong> total
-                  hospitalized cumulatively.
+                  <strong>
+                    {formatNumber(stateData[0].hospitalizedCumulative)}
+                  </strong>{' '}
+                  total hospitalized cumulatively.
                 </div>
               ) : (
                 ''
               )}
               {stateData[0].hospitalizedCurrently ? (
                 <div>
-                  <strong>{stateData[0].hospitalizedCurrently}</strong> are
-                  currently hospitalized.
+                  <strong>
+                    {formatNumber(stateData[0].hospitalizedCurrently)}
+                  </strong>{' '}
+                  are currently hospitalized.
                 </div>
               ) : (
                 ''
               )}
               {stateData[0].death ? (
                 <div>
-                  <strong>{stateData[0].death}</strong> have died.
+                  <strong>{formatNumber(stateData[0].death)}</strong> have died.
                 </div>
               ) : (
                 ''
               )}
               {stateData[0].recovered ? (
                 <div>
-                  <strong>{stateData[0].recovered}</strong> have recovered.
+                  <strong>{formatNumber(stateData[0].recovered)}</strong> have
+                  recovered.
                 </div>
               ) : (
                 'No info on recovery available at this time.'
--- src/components/USCards/USResultsCard.js
+++ src/components/USCards/USResultsCard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { USADataContext } from '../../utils/USAData';
+import formatNumber from '../formatNumber';
 
 function USResults() {
   const [USAData, setUSAData] = useContext(USADataContext);
@@ -11,41 +12,39 @@ function USResults() {
           <div>
             <div>
               <div>
-                <strong>{USAData[0].total}</strong> people have been tested.
+                <strong>{formatNumber(USAData[0].total)}</strong> people have
+                been tested.
               </div>
               <div>
-                <strong>{USAData[0].positive}</strong> people have tested
-                positive.
+                <strong>{formatNumber(USAData[0].positive)}</strong> people have
+                tested positive.
               </div>
               {USAData[0].hospitalizedCumulative ? (
                 <div>
-                  <strong>{USAData[0].hospitalizedCumulative}</strong> total
-                  hospitalized cumulatively.
+                  <strong>
+                    {formatNumber(USAData[0].hospitalizedCumulative)}
+                  </strong>{' '}
+                  total hospitalized cumulatively.
                 </div>
               ) : (
                 ''
               )}
               {USAData[0].hospitalizedCurrently ? (
                 <div>
-                  <strong>{USAData[0].hospitalizedCurrently}</strong> are
-                  currently hospitalized.
+                  <strong>
+                    {formatNumber(USAData[0].hospitalizedCurrently)}
+                  </strong>{' '}
+                  are currently hospitalized.
                 </div>
               ) : (
                 ''
               )}
               {USAData[0].death ? (
                 <div>
-                  <strong>{USAData[0].death}</strong> have died.
+                  <strong>{formatNumber(USAData[0].death)}</strong> have died.
                 </div>
               ) : (
                 ''
-              )}
-              {USAData[0].recovered ? (
-                <div>
-                  <strong>{USAData[0].recovered}</strong> have recovered.
-                </div>
-              ) : (
-                'No info on recovery available at this time.'
               )}
             </div>
           </div>
