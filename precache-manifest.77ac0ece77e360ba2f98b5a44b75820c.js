a078f5fb5905ee90a2934a0543eec382a73119fe
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-04-10 13:42:16 -0500

    Updates

--- precache-manifest.77ac0ece77e360ba2f98b5a44b75820c.js
+++ precache-manifest.77ac0ece77e360ba2f98b5a44b75820c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "72165af0a528f74d310fac2cb80c0183",
+    "revision": "8294c9da6035e97d64d02a65ea3a7624",
     "url": "/covid-19/index.html"
   },
   {
-    "revision": "dab76cdc78d92a889b28",
-    "url": "/covid-19/static/css/main.100e6976.chunk.css"
+    "revision": "5f823f84beda1a8be8d9",
+    "url": "/covid-19/static/css/main.a4e5566d.chunk.css"
   },
   {
     "revision": "fb58da8117e75963c115",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-19/static/js/2.7dba55bd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dab76cdc78d92a889b28",
-    "url": "/covid-19/static/js/main.208b96db.chunk.js"
+    "revision": "5f823f84beda1a8be8d9",
+    "url": "/covid-19/static/js/main.fd86a49d.chunk.js"
   },
   {
     "revision": "1b48a8de8cf756029d8c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19/precache-manifest.02491b9916cabf8c17b47b57416300a5.js"
+  "/covid-19/precache-manifest.77ac0ece77e360ba2f98b5a44b75820c.js"
 );
 
 self.addEventListener('message', (event) => {
